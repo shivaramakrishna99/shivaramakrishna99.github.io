59104a6054cad5bdef260155ed0a62df7ca97276
Shivaramakrishna Srinivasan <shivaramakrishna.srinivasan@gmail.com>
2024-12-24 15:42:52 -0800

    nuxt images and nuxt font configured right

--- my-app/layouts/default.vue
+++ my-app/layouts/default.vue
@@ -1,20 +1,25 @@
 <template>
     <div>
-      <Nav />
-      <main>
-        <slot />
-      </main>
-      <Footer />
+        <Nav />
+        <main class="container mx-auto max-w-3xl p-4">
+            <slot />
+        </main>
+        <Footer />
     </div>
-  </template>
-  
-  <script setup>
-  import Nav from '~/components/Nav.vue';
-  import Footer from '~/components/Footer.vue';
-  </script>
-  
-  <style scoped>
-  main {
-    min-height: calc(100vh - 100px); /* Adjust based on nav/footer height */
-  }
-  </style>
+</template>
+
+<script setup>
+import Nav from '~/components/Nav.vue';
+import Footer from '~/components/Footer.vue';
+</script>
+
+<style scoped>
+main {
+    min-height: calc(100vh - 100px);
+    /* Adjust based on nav/footer height */
+}
+
+div {
+    font-family: Fraunces, sans-serif;
+}
+</style>
--- my-app/nuxt.config.ts
+++ my-app/nuxt.config.ts
@@ -2,5 +2,15 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/content', '@nuxt/fonts', '@nuxtjs/tailwindcss']
-})
+  modules: [
+    '@nuxt/content',
+    '@nuxt/fonts',
+    '@nuxtjs/tailwindcss',
+    '@nuxt/image'
+  ],
+  app: {
+    pageTransition: { name: 'page', mode: 'out-in' }
+  },
+  css: ['@/assets/css/main.css'],
+}
+)

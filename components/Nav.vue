e0d9cebf8e1f28bf769a13782fa02ccc67f85a28
Shivaramakrishna Srinivasan <shivaramakrishna.srinivasan@gmail.com>
2024-12-29 14:11:52 -0800

    nav style changes, inclusion of italic fonts

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,18 +1,18 @@
 <template>
   <header class="bg-yellow-50">
-    <div class="layout-container flex flex-col items-center sm:flex-row sm:justify-between">
+    <div class="layout-container flex flex-col space-y-2 items-start sm:items-center sm:flex-row sm:justify-between">
     <NuxtLink to="/">
     <div class="flex items-center">
-      <NuxtImg src="/img/utils/srks.png" alt="Logo" class="h-8 w-8 mr-2" />
+      <NuxtImg src="/img/utils/srks.png" alt="Logo" class="h-10 w-10 mr-2" />
       <span class="text-xl font-bold">Shivaramakrishna</span>
     </div>
   </NuxtLink>
-    <nav class="flex space-x-4">
+    <nav class="flex items-start space-x-4">
       <NuxtLink
         v-for="(item, index) in navItems"
         :key="index"
         :to="item.url"
-        class="rounded-lg px-3 py-2 sm:py-10 font-sans font-extrabold text-slate-700 hover:text-sky-700 transition duration-300 ease-in-out"
+        class="rounded-lg pr-3 py-2 sm:py-10 font-sans font-extrabold text-slate-700 hover:text-sky-700 transition duration-300 ease-in-out"
       >
         {{ item.title }}
       </NuxtLink>

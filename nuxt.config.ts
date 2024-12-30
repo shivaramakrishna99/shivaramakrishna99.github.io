fd4adb113497f1d2178851543a760b0cf8748210
Shivaramakrishna Srinivasan <shivaramakrishna.srinivasan@gmail.com>
2024-12-29 20:36:09 -0800

    added basic seo and meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,8 @@
 export default defineNuxtConfig({
   target: 'static',
   router: {
-     base: '/'
-    //  buildAssetsDir: '/'
+    base: '/'
+    // buildAssetsDir: '/'
   },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
@@ -26,13 +26,34 @@ export default defineNuxtConfig({
       { name: 'Satoshi', provider: 'fontshare' }, // sans
       { name: 'Sentinel', provider: 'fontshare' }, // serif
       { name: 'Work Sans', provider: 'fontshare' }, // display
-      { name: 'JetBrains Mono', provider: 'fontshare' }, // mono
+      { name: 'JetBrains Mono', provider: 'fontshare' } // mono
     ]
   },
   content: {
     highlight: {
       theme: 'github-light', // Optional: Choose a code highlighting theme
     }
+  },
+  head: {
+    title: 'My Website',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'In a scene 10m wide among the many Powers of Ten' },
+      { name: 'keywords', content: 'nuxt, vue, javascript, awesome' },
+      { name: 'author', content: 'Shivaramakrishna Srinivasan' },
+      { name: 'robots', content: 'index, follow' },
+      { name: 'og:title', content: 'Shivaramakrishna Srinivasan' },
+      { name: 'og:description', content: 'In a scene 10m wide among the many Powers of Ten' },
+      // { name: 'og:image', content: '/path/to/og-image.png' },
+      { name: 'og:url', content: 'https://shivaramakrishna99.github.io' },
+      // { name: 'twitter:card', content: 'summary_large_image' },
+      { name: 'twitter:title', content: 'Shivaramakrishna Srinivasan' },
+      { name: 'twitter:description', content: 'In a scene 10m wide among the many Powers of Ten' },
+      // { name: 'twitter:image', content: '/path/to/twitter-image.png' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
   }
-}
-)
+});

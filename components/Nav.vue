ac093ba15c88d0c6b08e7dbe6fc598ae7f26388c
Shivaramakrishna Srinivasan <shivaramakrishna.srinivasan@gmail.com>
2025-04-02 14:26:32 -0700

    glass effect for top nav

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,16 +1,17 @@
 <template>
-  <header class="bg-flexoki-paper">
+  <header>
+    <div class="bg-flexoki-light-bg dark:bg-flexoki-dark-bg-2 bg-opacity-50 dark:bg-opacity-50 backdrop-blur-md dark:backdrop-blur bg-clip-padding border-none sm:border-2 border-flexoki-light-bg-2 dark:border-flexoki-dark-bg-2 rounded-3xl mt-2 sm:mt-6 py-2 sm:py-6 mx-auto max-w-5xl">
     <div
-      class="layout-container py-12 flex flex-col space-y-2 items-start sm:items-center sm:flex-row sm:justify-between"
+    class="layout-container  flex flex-col space-y-2 items-start sm:items-center sm:flex-row sm:justify-between"
     >
       <NuxtLink to="/">
         <div class="flex items-center">
           <NuxtImg
             src="/img/utils/srks.png"
             alt="Logo"
-            class="h-10 w-10 mr-2"
+            class="h-10 w-10 mr-2 rounded-full shadow-none"
           />
-          <span class="text-xl font-display">Shivaramakrishna Srinivasan</span>
+          <span class="text-xl font-title font-medium">Shivaramakrishna Srinivasan</span>
         </div>
       </NuxtLink>
       <nav class="flex items-start space-x-4">
@@ -18,12 +19,13 @@
           v-for="(item, index) in navItems"
           :key="index"
           :to="item.url"
-          class="rounded-lg pr-3 hover:text-cyan-500 transition duration-300 ease-in-out"
+          class="rounded-lg pr-4 font-normal hover:text-flexoki-light-bl transition duration-300 ease-in-out"
         >
           {{ item.title }}
         </NuxtLink>
       </nav>
     </div>
+  </div>
   </header>
 </template>
 
@@ -32,9 +34,9 @@ export default {
   data() {
     return {
       navItems: [
-        { title: 'About', url: '/about' },
-        { title: 'Writing', url: '/writing' },
         { title: 'Projects', url: '/projects' },
+        { title: 'Writing', url: '/writing' },
+        // { title: 'Resume', url: '/resume' },
       ],
     }
   },

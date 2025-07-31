b173ffec7ddbf56fc5e68e739e947555e7029760
Shivaramakrishna Srinivasan <shivaramakrishna.srinivasan@gmail.com>
2025-07-30 20:39:17 -0700

    style fixes for projects and writing homepages

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,32 +1,51 @@
 <template>
   <div>
-    <h3>Projects</h3>
+    <section>
+      <div class="text-center my-8 sm:my-12">
+        <h1 class="not-italic">Projects</h1>
+        <p class="italic font-serif">some not-so-messy ideas</p>
+      </div>
+    </section>
 
-<div v-if="projects.length" class="masonry-grid gap-6">
+    <section>
       <div
-        v-for="project in projects"
-        :key="project.slug"
-        class="break-inside-avoid mb-6 rounded-2xl border border-flexoki-light-accent bg-flexoki-light-bg hover:border-flexoki-light-bl hover:bg-white transition-all shadow-sm hover:shadow-md p-6"
+        class="grid gap-4 grid-cols-1 sm:grid-cols-2 3xl:grid-cols-3"
+        v-if="projects.length"
       >
-        <NuxtLink :to="`/projects/${project.slug}`" class="block group">
-          <h3 class="text-xl font-semibold text-flexoki-light-tx group-hover:text-flexoki-light-bl transition">
+        <NuxtLink
+          v-for="project in projects"
+          :key="project.slug"
+          :to="`/projects/${project.slug}`"
+          class="
+          block group h-auto sm:h-48 border-2
+          rounded-3xl px-8 py-12
+          bg-flexoki-base-50 dark:bg-flexoki-base-900
+          border-flexoki-base-100 dark:border-flexoki-base-950
+          hover:bg-flexoki-paper dark:hover:bg-flexoki-dark-bg-2
+          hover:border-flexoki-dark-pu-2 dark:hover:border-flexoki-light-pu-2 
+          transition
+          "
+        >
+          <h3 class="
+          font-classic text-2xl sm:text-4xl italic 
+          group-hover:text-flexoki-dark-pu-2 dark:group-hover:text-flexoki-light-pu-2
+          transition
+          ">
             {{ project.title }}
           </h3>
-          <p class="text-sm text-flexoki-light-muted mt-2 whitespace-pre-line">
-            {{ project.description || 'No description available yet.' }}
-          </p>
         </NuxtLink>
+
+
       </div>
-    </div>
 
-
-    <div v-else class="text-center py-12 text-flexoki-light-muted">
-      <div class="text-4xl mb-4">🚧</div>
-      <h3 class="text-lg">
-        Projects are being written up! <br />
-        Check back soon for some cool things I've done.
-      </h3>
-    </div>
+      <div class="text-center" v-else>
+        <h3>🚧</h3>
+        <h3>
+          Projects are typed up! <br />
+          Check back soon for some cool things I've done.
+        </h3>
+      </div>
+    </section>
   </div>
 </template>
 
--- pages/writing/index.vue
+++ pages/writing/index.vue
@@ -1,34 +1,51 @@
 <template>
   <div>
-    <h3>Writing</h3>
-    <ul class="space-y-4 list-none indent-0" v-if="articles.length">
-      <li
-        class="hover:text-flexoki-light-bl transition duration-300 ease-in-out"
-        v-for="article in articles"
-        :key="article.slug"
+    <section>
+      <div class="text-center my-8 sm:my-12">
+        <h1 class="not-italic">Writing</h1>
+        <p class="italic font-serif">some messy thoughts</p>
+      </div>
+    </section>
+
+    <section>
+      <div
+        class="grid gap-4 grid-cols-1 sm:grid-cols-2 3xl:grid-cols-3"
+        v-if="articles.length"
       >
-      <span class="font-mono">
-      {{
-          article.created
-          }}
-          &#8226;
-        </span>
-        <span class="underline">
-        <NuxtLink :to="`/writing/${article.slug}`">
-          {{
-          article.title
-          }}
+        <NuxtLink
+          v-for="article in articles"
+          :key="article.slug"
+          :to="`/writing/${article.slug}`"
+          class="
+          block group h-auto sm:h-96 border-2
+          rounded-3xl px-8 py-12
+          bg-flexoki-base-50 dark:bg-flexoki-base-900
+          border-flexoki-base-100 dark:border-flexoki-base-950
+          hover:bg-flexoki-paper dark:hover:bg-flexoki-dark-bg-2
+          hover:border-flexoki-dark-bl-2 dark:hover:border-flexoki-light-bl-2 
+          transition
+          "
+        >
+          <h3 class="
+          font-classic text-2xl sm:text-4xl italic
+          group-hover:text-flexoki-dark-bl-2 dark:group-hover:text-flexoki-light-bl-2
+          transition
+          ">
+            {{ article.title }}
+          </h3>
         </NuxtLink>
-      </span> 
-      </li>
-    </ul>
-    <div class="text-center" v-else>
-      <h3>🚧</h3>
-      <h3>
-        Articles are being written! <br />
-        Check back soon for some cool things I've done.
-      </h3>
-    </div>
+
+
+      </div>
+
+      <div class="text-center" v-else>
+        <h3>🚧</h3>
+        <h3>
+          Articles are being written! <br />
+          Check back soon for some cool things I've done.
+        </h3>
+      </div>
+    </section>
   </div>
 </template>
 

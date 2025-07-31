cc20e61ed8a7e016d10226580488cf414c4f028a
Shivaramakrishna Srinivasan <shivaramakrishna.srinivasan@gmail.com>
2025-07-30 20:39:28 -0700

    style fixes for project entries

--- pages/projects/[slug].vue
+++ pages/projects/[slug].vue
@@ -1,22 +1,35 @@
 <template>
-  <div>
-    <ContentDoc :document="project" />
-  </div>
+  <main>
+    <ContentDoc>
+      <template v-slot="{ doc }">
+        <article>
+          <h2 class="font-mono not-italic py-8 sm:py-12">{{ doc.title }}</h2>
+          <h5>{{ doc.description }}</h5>
+          <ContentRenderer :value="doc" />
+        </article>
+      </template>
+      <template #not-found>
+        <h1>Document not found</h1>
+      </template>
+    </ContentDoc>
+  </main>
 </template>
 
 <script setup>
 // Get the slug from the route
 const route = useRoute()
 const slug = route.params.slug
+const path = route.fullPath
+console.log("FULL PATH", path)
 
 // Fetch the content document
-const project = await queryContent('projects').where({ slug }).findOne()
+const doc = await queryContent('projects').where({ slug }).findOne()
 
-if (!project) {
+if (!doc) {
   // Handle 404 if article is not found
   throw createError({
     statusCode: 404,
-    statusMessage: 'Project not found',
+    statusMessage: 'Article not found',
   })
 }
 </script>
